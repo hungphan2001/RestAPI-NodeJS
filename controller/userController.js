c4dd3bf7057b2789e99d3fbe0333882fc2b58dc0
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-05-05 14:48:44 +0700

    nodejs:create project nodejs
    connect mongodb
    
    check status 200 ? 500 : res.code
    
    try use postman post api and check api
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

---
+++ controller/userController.js
@@ -0,0 +1,15 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const createUser = asyncHandler(async (req,res)=>{
+    const email = req.body.email;
+    const findUser = await User.findOne({email:email});
+    if(!findUser){
+        //Create a new user
+        const newUser = User.create(req.body);
+        res.json(newUser);
+    } else{
+        throw new Error("User Already Exist");
+    }
+});
+module.exports= {createUser};
---
+++ routes/authRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const {createUser} = require("../controller/userController");
+const router= express.Router();
+router.post("/signup",createUser);
+module.exports = router;
