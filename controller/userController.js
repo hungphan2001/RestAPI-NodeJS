817d63f28db3767b06aec0af456b5b2b7a280f23
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-05-08 16:47:11 +0700

    get/update/delete from user
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- controller/userController.js
+++ controller/userController.js
@@ -2,33 +2,33 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const { generateToken } = require("../config/jwtToken");
 
-const createUser = asyncHandler(async (req,res)=>{
+const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
-    const findUser = await User.findOne({email:email});
-    if(!findUser){
+    const findUser = await User.findOne({ email: email });
+    if (!findUser) {
         //Create a new user
         const newUser = await User.create(req.body);
         res.json(newUser);
-    } else{
+    } else {
         throw new Error("User Already Exist");
     }
 });
 
-const loginUserController = asyncHandler(async (req,res)=>{
-    const {email,password}= req.body;
+const loginUserController = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
     //check if user exists or not
-    const findUser = await User.findOne({email});
-    if(findUser &&(await findUser.isPasswordMatched(password))){
+    const findUser = await User.findOne({ email });
+    if (findUser && (await findUser.isPasswordMatched(password))) {
         res.json({
-            _id:findUser?._id,
-            firstname:findUser?.firstname,
-            lastname:findUser?.lastname,
-            email:findUser?.email,
-            mobile:findUser?.mobile,
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
             //role:findUser?.role,
-            token:generateToken(findUser?._id),
+            token: generateToken(findUser?._id),
         });
-    } else{
+    } else {
         throw new Error("Invalid Credentials")
     }
     //console.log(email,password);
@@ -36,12 +36,71 @@ const loginUserController = asyncHandler(async (req,res)=>{
 
 //Get/Read all users
 
-const getAllUser = asyncHandler(async (req,res)=>{
+const getAllUser = asyncHandler(async (req, res) => {
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+//Get a single user
+
+const getAUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const getAUser = await User.findById(id);
+        res.json({
+            getAUser,
+        });
+        console.log(id);
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+//Update a user 
+const updateUser = asyncHandler(async(req,res)=>{
+    const {id} = req.params;
     try{
-   const getUsers = await User.find();
-   res.json(getUsers);
+      const updateUser = await User.findByIdAndUpdate(
+        id,
+        {
+            firstname: req?.body?.firstname,
+            lastname: req?.body?.lastname,
+            email: req?.body?.email,
+            mobile: req?.body?.mobile, 
+        },{
+            new:true,
+        }
+        );
+        res.json({
+            updateUser,
+        })
     } catch (error){
         throw new Error(error);
     }
 })
-module.exports= {createUser,loginUserController,getAllUser};
+
+//Delete a user
+
+const deleteAUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleteUser = await User.findByIdAndDelete(id);
+        res.json({
+            deleteUser,
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+module.exports = { 
+    createUser, 
+    loginUserController, 
+    getAllUser, 
+    getAUser,
+    updateUser, 
+    deleteAUser 
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const {createUser,loginUserController,getAllUser} = require("../controller/userController");
+const {createUser,loginUserController,getAllUser,getAUser,updateUser,deleteAUser} = require("../controller/userController");
 const router= express.Router();
 router.post("/signup",createUser);
 router.post("/login",loginUserController);
 router.get("/all-users",getAllUser);
+router.get("/:id",getAUser);
+router.put("/:id",updateUser);
+router.delete("/:id",deleteAUser);
 module.exports = router;
