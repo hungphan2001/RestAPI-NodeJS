2abea9a40e354d7c343a96448ccc4fa34f21904a
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-05-08 16:09:05 +0700

    create jwt json token for id and get data from users
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const { generateToken } = require("../config/jwtToken");
 
 const createUser = asyncHandler(async (req,res)=>{
     const email = req.body.email;
@@ -18,10 +19,29 @@ const loginUserController = asyncHandler(async (req,res)=>{
     //check if user exists or not
     const findUser = await User.findOne({email});
     if(findUser &&(await findUser.isPasswordMatched(password))){
-        res.json(findUser);
+        res.json({
+            _id:findUser?._id,
+            firstname:findUser?.firstname,
+            lastname:findUser?.lastname,
+            email:findUser?.email,
+            mobile:findUser?.mobile,
+            //role:findUser?.role,
+            token:generateToken(findUser?._id),
+        });
     } else{
         throw new Error("Invalid Credentials")
     }
     //console.log(email,password);
 })
-module.exports= {createUser,loginUserController};
+
+//Get/Read all users
+
+const getAllUser = asyncHandler(async (req,res)=>{
+    try{
+   const getUsers = await User.find();
+   res.json(getUsers);
+    } catch (error){
+        throw new Error(error);
+    }
+})
+module.exports= {createUser,loginUserController,getAllUser};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const {createUser,loginUserController} = require("../controller/userController");
+const {createUser,loginUserController,getAllUser} = require("../controller/userController");
 const router= express.Router();
 router.post("/signup",createUser);
 router.post("/login",loginUserController);
+router.get("/all-users",getAllUser);
 module.exports = router;
